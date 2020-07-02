d40021086c302d37585c066e1085c845b641cb0d
DESKTOP1_Tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2020-07-02 21:19:53 +0900

    for文で回すのを削除

--- camImageOffsetConnection.py
+++ camImageOffsetConnection.py
@@ -4,11 +4,9 @@ def main():
     camImageOffsetConnection()
 
 def camImageOffsetConnection():
-    cam = cmds.ls(sl=1,dag=1,typ = 'camera')
-    connections = cmds.listConnections(cam,t = 'imagePlane')
-    imagePs =  []
-    for connection in connections:
-        imagePs.append(connection.replace(cam[0]+'->',''))
-    
-    for imageP in imagePs:
-        cmds.connectAttr(cam[0]+'.filmOffset',imageP+'.offset')
+    cam = cmds.ls(sl = True, dag = True, typ = 'camera')
+    connection = cmds.listConnections(cam, t = 'imagePlane')
+
+    imageP = connection[0].replace( cam[0] + '->', '' )
+
+    cmds.connectAttr(cam[0] + '.filmOffset', imageP + '.offset')
