153e4b69d2d77eb731a13beae6c56a297df94e33
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2020-07-07 14:21:22 +0900

    '_'で区切られていなくても実行できるように修正完了。

--- extractComponent.py
+++ extractComponent.py
@@ -13,13 +13,19 @@ def main():
     num = '1'.zfill(3)
     selFace = pm.selected()
     shape = pm.listRelatives(selFace, p = True)
-    obj = pm.listRelatives(shape, p = True) 
+    obj = pm.listRelatives(shape, p = True)
+    part = obj[0].split("_")
     pm.polyChipOff(selFace, dup=False )
     pm.polySeparate(obj)
     pm.delete(ch = True)
     childs = pm.listRelatives( obj, c = True )
     for child in childs:
-        pm.rename(child , '_'.join(['geo','model',scene,num]) )
+        print len(part)
+        if len(part) == 1:
+            pm.rename(child , '_'.join(['geo',part[0],scene,num]) )
+        else :
+            pm.rename(child , '_'.join(['geo',part[1],scene,num]) )
+
         if pm.objExists( '_'.join(['grp','geo',scene]) ):
             pm.parent(child, '_'.join(['grp','geo',scene]) )
         else :
